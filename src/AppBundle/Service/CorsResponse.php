518c978c30e7271d7d8af78200d54f6c4aaf9867
endi <endi1982@gmail.com>
2016-05-06 17:51:30 +0300

    add cros service

---
+++ src/AppBundle/Service/CorsResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Service;
+
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class CorsResponse
+{
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $responseHeaders = $event->getResponse()->headers;
+
+        $responseHeaders->set('Access-Control-Allow-Headers', 'origin, content-type, accept');
+        $responseHeaders->set('Access-Control-Allow-Origin', '*');
+        $responseHeaders->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
+    }
+}
